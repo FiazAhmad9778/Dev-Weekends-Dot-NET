2f3d5fe56f6f8202228c5bd38412b60553ecbafb
fiazahmad <fiazahmad1g@gmail.com>
2022-06-12 17:25:28 +0500

    consrctors session

---
+++ MemoryAndConstructors/Constructor/Parent.cs
@@ -0,0 +1,55 @@
+﻿namespace Constructor;
+
+//static
+//Embeded
+//Parent
+//Child
+
+
+//Inner
+//Parent
+//Child
+
+
+public class Parent
+{
+    public Parent()
+    {
+        Console.WriteLine("Parent");
+    }
+    static Parent()
+    {
+        Console.WriteLine("static");
+    }
+}
+public class Child : Parent
+{
+    public Child()
+    {
+        Console.WriteLine("Child");
+    }
+    static Child()
+    {
+        Console.WriteLine("child");
+    }
+}
+
+
+public class Memory
+{
+    public int a { get; set; }
+    public string s { get; set; }
+    public static int staticvalue = 1;
+}
+
+Memory m = new Memory();
+
+
+
+public class Inner
+{
+    public Inner()
+    {
+        Console.WriteLine("Inner");
+    }
+}
---
+++ MemoryAndConstructors/Program.cs
@@ -0,0 +1,13 @@
+﻿using Constructor;
+
+
+//Child child = new Child();
+//Child child1 = new Child();
+
+//thread
+Child child1 = new Child();
+
+//thread
+Child child2 = new Child();
+
+
